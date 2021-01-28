174aeb3e13c73b963589c26725e36fcc3c3f3f57
Дiegushko Хименес <dkhimenesguakhardo@edu.hse.ru>
2021-01-29 01:09:00 +0300

    CycleGan Testing

---
+++ test.py
@@ -0,0 +1,234 @@
+import natsort
+import numpy as np
+import glob
+import random
+import os
+from PIL import Image
+import torch
+from torch.utils.data import Dataset
+from torch import nn
+from tqdm.auto import tqdm
+import torchvision
+from torchvision import transforms
+from torchvision.utils import make_grid
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+
+weights_ = '/home/diegushko/checkpoint/cezanne2photo/'
+output_ = '/home/diegushko/github/CycleGAN/out_images/'
+test_path = '/home/diegushko/github/CycleGAN/test_images/'
+
+adv_criterion = nn.MSELoss() 
+recon_criterion = nn.L1Loss() 
+
+n_epochs = 20
+dim_A = 3
+dim_B = 3
+display_step = 200
+batch_size = 1
+lr = 0.0002
+load_shape = 286
+target_shape = 256
+device = 'cuda'
+
+class ResidualBlock(nn.Module):
+
+    def __init__(self, input_channels):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(input_channels, input_channels, kernel_size=3, padding=1, padding_mode='reflect')
+        self.conv2 = nn.Conv2d(input_channels, input_channels, kernel_size=3, padding=1, padding_mode='reflect')
+        self.instancenorm = nn.InstanceNorm2d(input_channels)
+        self.activation = nn.ReLU()
+
+    def forward(self, x):
+
+        original_x = x.clone()
+        x = self.conv1(x)
+        x = self.instancenorm(x)
+        x = self.activation(x)
+        x = self.conv2(x)
+        x = self.instancenorm(x)
+        return original_x + x
+
+class ContractingBlock(nn.Module):
+
+    def __init__(self, input_channels, use_bn=True, kernel_size=3, activation='relu'):
+        super(ContractingBlock, self).__init__()
+        self.conv1 = nn.Conv2d(input_channels, input_channels * 2, kernel_size=kernel_size, padding=1, stride=2, padding_mode='reflect')
+        self.activation = nn.ReLU() if activation == 'relu' else nn.LeakyReLU(0.2)
+        if use_bn:
+            self.instancenorm = nn.InstanceNorm2d(input_channels * 2)
+        self.use_bn = use_bn
+
+    def forward(self, x):
+
+        x = self.conv1(x)
+        if self.use_bn:
+            x = self.instancenorm(x)
+        x = self.activation(x)
+        return x
+
+class ExpandingBlock(nn.Module):
+
+    def __init__(self, input_channels, use_bn=True):
+        super(ExpandingBlock, self).__init__()
+        self.conv1 = nn.ConvTranspose2d(input_channels, input_channels // 2, kernel_size=3, stride=2, padding=1, output_padding=1)
+        if use_bn:
+            self.instancenorm = nn.InstanceNorm2d(input_channels // 2)
+        self.use_bn = use_bn
+        self.activation = nn.ReLU()
+
+    def forward(self, x):
+
+        x = self.conv1(x)
+        if self.use_bn:
+            x = self.instancenorm(x)
+        x = self.activation(x)
+        return x
+
+class FeatureMapBlock(nn.Module):
+
+    def __init__(self, input_channels, output_channels):
+        super(FeatureMapBlock, self).__init__()
+        self.conv = nn.Conv2d(input_channels, output_channels, kernel_size=7, padding=3, padding_mode='reflect')
+
+    def forward(self, x):
+
+        x = self.conv(x)
+        return x
+
+class Generator(nn.Module):
+
+    def __init__(self, input_channels, output_channels, hidden_channels=64):
+        super(Generator, self).__init__()
+        self.upfeature = FeatureMapBlock(input_channels, hidden_channels)
+        self.contract1 = ContractingBlock(hidden_channels)
+        self.contract2 = ContractingBlock(hidden_channels * 2)
+        res_mult = 4
+        self.res0 = ResidualBlock(hidden_channels * res_mult)
+        self.res1 = ResidualBlock(hidden_channels * res_mult)
+        self.res2 = ResidualBlock(hidden_channels * res_mult)
+        self.res3 = ResidualBlock(hidden_channels * res_mult)
+        self.res4 = ResidualBlock(hidden_channels * res_mult)
+        self.res5 = ResidualBlock(hidden_channels * res_mult)
+        self.res6 = ResidualBlock(hidden_channels * res_mult)
+        self.res7 = ResidualBlock(hidden_channels * res_mult)
+        self.res8 = ResidualBlock(hidden_channels * res_mult)
+        self.expand2 = ExpandingBlock(hidden_channels * 4)
+        self.expand3 = ExpandingBlock(hidden_channels * 2)
+        self.downfeature = FeatureMapBlock(hidden_channels, output_channels)
+        self.tanh = torch.nn.Tanh()
+
+    def forward(self, x):
+
+        x0 = self.upfeature(x)
+        x1 = self.contract1(x0)
+        x2 = self.contract2(x1)
+        x3 = self.res0(x2)
+        x4 = self.res1(x3)
+        x5 = self.res2(x4)
+        x6 = self.res3(x5)
+        x7 = self.res4(x6)
+        x8 = self.res5(x7)
+        x9 = self.res6(x8)
+        x10 = self.res7(x9)
+        x11 = self.res8(x10)
+        x12 = self.expand2(x11)
+        x13 = self.expand3(x12)
+        xn = self.downfeature(x13)
+        return self.tanh(xn)
+
+class Discriminator(nn.Module):
+
+    def __init__(self, input_channels, hidden_channels=64):
+        super(Discriminator, self).__init__()
+        self.upfeature = FeatureMapBlock(input_channels, hidden_channels)
+        self.contract1 = ContractingBlock(hidden_channels, use_bn=False, kernel_size=4, activation='lrelu')
+        self.contract2 = ContractingBlock(hidden_channels * 2, kernel_size=4, activation='lrelu')
+        self.contract3 = ContractingBlock(hidden_channels * 4, kernel_size=4, activation='lrelu')
+        self.final = nn.Conv2d(hidden_channels * 8, 1, kernel_size=1)
+
+    def forward(self, x):
+        x0 = self.upfeature(x)
+        x1 = self.contract1(x0)
+        x2 = self.contract2(x1)
+        x3 = self.contract3(x2)
+        xn = self.final(x3)
+        return xn
+
+
+gen_AB = Generator(dim_A, dim_B).to(device)
+gen_BA = Generator(dim_B, dim_A).to(device)
+gen_opt = torch.optim.Adam(list(gen_AB.parameters()) + list(gen_BA.parameters()), lr=lr, betas=(0.5, 0.999))
+disc_A = Discriminator(dim_A).to(device)
+disc_A_opt = torch.optim.Adam(disc_A.parameters(), lr=lr, betas=(0.5, 0.999))
+disc_B = Discriminator(dim_B).to(device)
+disc_B_opt = torch.optim.Adam(disc_B.parameters(), lr=lr, betas=(0.5, 0.999))
+
+def weights_init(m):
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+    if isinstance(m, nn.BatchNorm2d):
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+        torch.nn.init.constant_(m.bias, 0)
+
+pretrained = True
+if pretrained:
+    pre_dict = torch.load(weights_ + 'cycleGAN_99.pth')
+    gen_AB.load_state_dict(pre_dict['gen_AB'])
+    gen_BA.load_state_dict(pre_dict['gen_BA'])
+    gen_opt.load_state_dict(pre_dict['gen_opt'])
+    disc_A.load_state_dict(pre_dict['disc_A'])
+    disc_A_opt.load_state_dict(pre_dict['disc_A_opt'])
+    disc_B.load_state_dict(pre_dict['disc_B'])
+    disc_B_opt.load_state_dict(pre_dict['disc_B_opt'])
+else:
+    gen_AB = gen_AB.apply(weights_init)
+    gen_BA = gen_BA.apply(weights_init)
+    disc_A = disc_A.apply(weights_init)
+    disc_B = disc_B.apply(weights_init)
+
+batch_no = 1
+
+class CustomDataSet(Dataset):
+    def __init__(self, main_dir, transform):
+        self.main_dir = main_dir
+        self.transform = transform
+        all_imgs = os.listdir(main_dir)
+        self.total_imgs = natsort.natsorted(all_imgs)
+
+    def __len__(self):
+        return len(self.total_imgs)
+
+    def __getitem__(self, idx):
+        img_loc = os.path.join(self.main_dir, self.total_imgs[idx])
+        image = Image.open(img_loc).convert("RGB")
+        tensor_image = self.transform(image)
+        return tensor_image
+
+means = np.array((0.5, 0.5, 0.5))
+stds = np.array((0.5, 0.5, 0.5))
+
+transform_test = transforms.Compose([transforms.Resize((256, 256)),
+                               transforms.ToTensor(),
+                               transforms.Normalize(means, stds)
+                               ])
+
+test_set = CustomDataSet(test_path, transform=transform_test)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_no, shuffle=False)
+
+with torch.no_grad():
+    for idx, img in enumerate(test_loader):
+        real_b = nn.functional.interpolate(img, size=target_shape)
+        real_b = real_b.to(device)
+        fake_a = gen_BA(real_b)
+        catenation = torch.cat([real_b, fake_a])
+        image_tensor = (catenation + 1) / 2
+        image_shifted = image_tensor
+        image_unflat = image_shifted.detach().cpu().view(-1, *(dim_A, target_shape, target_shape))
+        image_grid = make_grid(image_unflat[:25], nrow=5)
+        plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+
+        plt.savefig(output_ + f'figure_{idx}.png')
+        plt.show()
